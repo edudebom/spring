3dd5bf5682da95a076e31348dc06afda71e2f6fb
Eduardo Debom <eduardodebom@hotmail.com>
2021-01-27 19:07:43 -0300

    projeto spring simples

---
+++ src/main/java/com/holomani/myfirst/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.holomani.myfirst.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.holomani.myfirst.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
---
+++ src/main/java/com/holomani/myfirst/repository/ProductRepository.java
@@ -0,0 +1,23 @@
+package com.holomani.myfirst.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.holomani.myfirst.entities.Product;
+
+@Component
+public class ProductRepository {
+
+	public void save(Product obj) {
+		
+	}
+
+	public Product findById(Long id) {
+		return null;
+	}
+	
+	public List<Product> findAll() {
+		return null;
+	}
+}
